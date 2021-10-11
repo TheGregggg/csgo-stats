3969fdafd2be90b58fbb76ccc946827042dedf96
Gregoire Layet <gregoirelayet@gmail.com>
2021-10-11 21:32:47 +0200

    begining project

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h2 class="logo">CSGO Stats</h2>
+        <ul>
+            <li class="link"><a href="">Accueil</a></li>
+            <li class="link"><a href="">Joueurs</a></li>
+        </ul>
+    </header>
+    <main>
+        <form action="/analyse_demo.php">
+            <input type="files" name="" id="">
+            <button action="validate">Validé</button>
+        </form>
+    </main>
+    <footer>
+        <p><a href="https://gregoirelayet.com">Grégoire Layet</a> & Tom Jegou</p>
+    </footer>
+</body>
+</html>
