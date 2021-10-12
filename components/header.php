7512138a9e1644bd3bacffb94bde2c02de31a764
Gregoire Layet <gregoirelayet@gmail.com>
2021-10-12 19:49:09 +0200

    css header

--- components/header.php
+++ components/header.php
@@ -1,7 +1,12 @@
 <header>
-    <h2 class="logo">CSGO Stats</h2>
-    <ul>
-        <li class="link"><a href="">Accueil</a></li>
-        <li class="link"><a href="">Joueurs</a></li>
-    </ul>
+    <div class="container">
+        <div class="flex">
+            <h2 class="logo">CSGO Stats</h2>
+            <ul class="links">
+                <li class="link"><a href="./">Accueil</a></li>
+                <li class="link"><a href="./joueur.php">Joueurs</a></li>
+            </ul>
+            <button>toggle</button>
+        </div>
+    </div>
 </header>
--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include './components/header_tags.php'; ?>
     <title>Document</title>
 </head>
 <body>
-
     <?php include './components/header.php'; ?>
 
-    <main>
+    <main class="container">
         <form action="/analyse_demo.php">
-            <input type="files" name="" id="">
+            <input type="file" name="" id="">
             <button action="validate">Validé</button>
         </form>
     </main>
 
     <?php include './components/footer.php'; ?>
-
 </body>
 </html>
