df26a48cfbe4472ca28faacd7d9faa8e0f695762
Gregoire LAYET <gregoire.layet@lyc-leonard-de-vinci-amboise.ac-orleans-tours.fr>
2021-10-15 14:52:02 +0200

    darkmode v0.1

--- components/header.php
+++ components/header.php
@@ -12,7 +12,30 @@
                 </ul>
             </span>
             <span class="right">
-                <button>toggle</button>
+                <button onclick="toggleDarkMode()">toggle</button>
+                <script>  
+                var darkMode = localStorage.getItem('darkMode')
+                if(darkMode == null){
+                    localStorage.setItem('darkMode', "false")
+                    darkMode = "false"
+                }
+                function toggleDarkMode(){
+                    darkMode = localStorage.getItem('darkMode')
+                    if(darkMode == "true"){
+                        localStorage.setItem('darkMode', "false")
+                    }else{
+                        localStorage.setItem('darkMode', "true")
+                    }
+                    var style = document.createElement('style');
+                    document.head.appendChild(style);
+                    if(darkMode == "true"){
+                        style.sheet.insertRule(":root {--main-bg-color: #333;--secondary-bg-color: #666;--main-fg-color: #fcfcfc;--secondary-fg-color: #ddd;--accent-color: rgb(211, 136, 255);--accent-color-darker: rgb(149, 96, 180);}");
+                    }else{
+                        style.sheet.insertRule(":root {--main-bg-color: #fff;--secondary-bg-color: #eee;--main-fg-color: #333;--secondary-fg-color: #ccc;--accent-color: rgb(211, 136, 255);--accent-color-darker: rgb(149, 96, 180);}");
+                    }
+                }
+                
+                </script>
             </span>
         </div>
     </div>
