394990ccb42998b88edd863fb154c301fd837374
gregoire <gregoirelayet@gmail.com>
2021-10-25 21:53:51 +0200

    remove .php in url

--- components/header.php
+++ components/header.php
@@ -6,8 +6,8 @@
             </span>
             <span class="center">
                 <ul class="links">
-                    <li class="link"><a href="./joueurs.php">Joueurs</a></li>
-                    <li class="link"><a href="./parties.php">Parties</a></li>
+                    <li class="link"><a href="./joueurs">Joueurs</a></li>
+                    <li class="link"><a href="./parties">Parties</a></li>
                 </ul>
             </span>
             <span class="right">
